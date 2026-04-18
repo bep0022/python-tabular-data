2ed247d85112ab7b3e35d61662be8501be122a70
Brynleigh Payne <bep0022@auburn.edu>
2026-04-18 12:47:41 -0500

    Stuck, but made importable

--- iris_species_plots.py
+++ iris_species_plots.py
@@ -1,58 +1,61 @@
-# Perform and plot linear regression for all three iris species: Iris_setosa, Iris_virginica, Iris_versicolor
+#! /usr/bin/env python3
+
+"Perform and plot linear regression for all three iris species: Iris_setosa, Iris_virginica, Iris_versicolor"
 
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
-# Import metadata
-dataframe = pd.read_csv("iris.csv")
+if __name__ == '__main__':
+    # Import metadata
+    dataframe = pd.read_csv("iris.csv")
 
-# Subset & plot Iris_setosa
-setosa = dataframe[dataframe.species == "Iris_setosa"]
-print(setosa)
+    # Subset & plot Iris_setosa
+    setosa = dataframe[dataframe.species == "Iris_setosa"]
+    print(setosa)
 
-x = setosa.petal_length_cm
-y = setosa.sepal_length_cm  
-setosa_regression = stats.linregress(x, y)
-setosa_slope = setosa_regression.slope
-setosa_intercept = setosa_regression.intercept
-plt.scatter(x, y, label = 'Setosa Data')  
-plt.plot(x, setosa_slope * x + setosa_intercept, color = "orange", label = 'Fitted line')
-plt.xlabel("Setosa Petal length (cm)")
-plt.ylabel("Setosa Sepal length (cm)")
-plt.legend()
-plt.savefig("setosa_petal_v_sepal_length_regress.png")
+    setosa_x = setosa.petal_length_cm
+    setosa_y = setosa.sepal_length_cm  
+    setosa_regression = stats.linregress(setosa_x, setosa_y)
+    setosa_slope = setosa_regression.slope
+    setosa_intercept = setosa_regression.intercept
+    plt.scatter(setosa_x, setosa_y, label = 'Setosa Data')  
+    plt.plot(setosa_x, setosa_slope * setosa_x + setosa_intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Setosa Petal length (cm)")
+    plt.ylabel("Setosa Sepal length (cm)")
+    plt.legend()
+    plt.savefig("setosa_petal_v_sepal_length_regress.png")
 
-# Subset & plot Iris_virginica
-virginica = dataframe[dataframe.species == "Iris_virginica"]
-print(virginica)
+    # Subset & plot Iris_virginica
+    virginica = dataframe[dataframe.species == "Iris_virginica"]
+    print(virginica)
 
-x = virginica.petal_length_cm
-y = virginica.sepal_length_cm  
-virginica_regression = stats.linregress(x, y)
-virginica_slope = virginica_regression.slope
-virginica_intercept = virginica_regression.intercept
-plt.scatter(x, y, label = 'Virginica Data')  
-plt.plot(x, virginica_slope * x + virginica_intercept, color = "orange", label = 'Fitted line')
-plt.xlabel("Virginica Petal length (cm)")
-plt.ylabel("Virginica Sepal length (cm)")
-plt.legend()
-plt.savefig("virginica_petal_v_sepal_length_regress.png")
+    virginica_x = virginica.petal_length_cm
+    virginica_y = virginica.sepal_length_cm  
+    virginica_regression = stats.linregress(virginica_x, virginica_y)
+    virginica_slope = virginica_regression.slope
+    virginica_intercept = virginica_regression.intercept
+    plt.scatter(virginica_x, virginica_y, label = 'Virginica Data')  
+    plt.plot(virginica_x, virginica_slope * virginica_x + virginica_intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Virginica Petal length (cm)")
+    plt.ylabel("Virginica Sepal length (cm)")
+    plt.legend()
+    plt.savefig("virginica_petal_v_sepal_length_regress.png")
 
-# Subset & plot Iris_versicolor
-versicolor = dataframe[dataframe.species == "Iris_versicolor"]
-print(versicolor)
+    # Subset & plot Iris_versicolor
+    versicolor = dataframe[dataframe.species == "Iris_versicolor"]
+    print(versicolor)
 
-x = versicolor.petal_length_cm
-y = versicolor.sepal_length_cm  
-versicolor_regression = stats.linregress(x, y)
-versicolor_slope = versicolor_regression.slope
-versicolor_intercept = versicolor_regression.intercept
-plt.scatter(x, y, label = 'Versicolor Data')  
-plt.plot(x, versicolor_slope * x + versicolor_intercept, color = "orange", label = 'Fitted line')
-plt.xlabel("Versicolor Petal length (cm)")
-plt.ylabel("Versicolor Sepal length (cm)")
-plt.legend()
-plt.savefig("versicolor_petal_v_sepal_length_regress.png")
+    versicolor_x = versicolor.petal_length_cm
+    versicolor_y = versicolor.sepal_length_cm  
+    versicolor_regression = stats.linregress(versicolor_x, versicolor_y)
+    versicolor_slope = versicolor_regression.slope
+    versicolor_intercept = versicolor_regression.intercept
+    plt.scatter(versicolor_x, versicolor_y, label = 'Versicolor Data')  
+    plt.plot(versicolor_x, versicolor_slope * versicolor_x + versicolor_intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Versicolor Petal length (cm)")
+    plt.ylabel("Versicolor Sepal length (cm)")
+    plt.legend()
+    plt.savefig("versicolor_petal_v_sepal_length_regress.png")
 
